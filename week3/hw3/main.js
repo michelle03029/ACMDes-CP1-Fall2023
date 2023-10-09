1b72b22420c1c5fc79daec6b325baa67e94f8e0a
Michelle Chen <43322845+michelle03029@users.noreply.github.com>
2023-10-09 09:39:30 -0700

    Added Reading Week Homework
    Quotes and Responses

--- week3/hw3/main.js
+++ week3/hw3/main.js
@@ -2,4 +2,14 @@ function myFunction(imgs) {
     var expandImg = document.getElementById("expandedImg");
     expandImg.src = imgs.src;
     expandImg.parentElement.style.display = "block";
+/*    console.log(event) */
+}
+
+let myinterval = window.setInterval(switchImage, 2000);
+if(undefined)[
+        
+]
+else {
+    document.getElementById("expandedImg").src=event.target.src
+}
 }
