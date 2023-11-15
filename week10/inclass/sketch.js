0400f07873452451d1759e8c576535fb986ed4dc
Michelle Chen <43322845+michelle03029@users.noreply.github.com>
2023-11-14 19:05:06 -0800

    changes to code

--- week10/inclass/sketch.js
+++ week10/inclass/sketch.js
@@ -2,15 +2,26 @@ let numRings = 10
 let radius
 let strWeight = 20
 
+let backColor
+
 function setup(){
     createCanvas(800, 800)
     colorMode(HSB, TWO_PI, 1, 1)
 
     radius = width * 0.1
+    backColor = color(0, 0, 0)
+
+    setTimeout(() => {
+        backColor = color(0, 0, 1)
+    }, 5000) //5000ms, change background from wblack to white
+
+    setInterval(() => {
+        backColor = color(random(TWO_PI), 0.8, 0.8)
+    }, 2000)
 }
 
 function draw(){
-    background(0)
+    background(backColor)
     stroke(255,0, 0)
     noFill()
     strokeWeight(strWeight)
@@ -20,10 +31,10 @@ function draw(){
         //translate to the center of screen
         translate(width*0.5, height*0.5) 
         //tamer towards the middle, last number is amplitude
-        rotate(sin(millis() * 0.001 * (i*0.5+1) * 0.5))
-        stroke(i*TWO_PI/numRings, 0.8, 0.8)
+        rotate(sin(millis() * 0.001 * (i * 0.5 + 1) * 0.5))
+        stroke(i * TWO_PI/numRings, 0.8, 0.8)
         //draw around 0, 0 points
-        arc(0, 0, radius*2 + i*strWeight*2, radius*2 + i*strWeight*2, PI*0.75, PI*0.25) 
+        arc(0, 0, radius * 2 + i * strWeight * 2, radius * 2 + i * strWeight*2, PI * 0.75, PI * 0.25) 
         pop()
     }
 }
