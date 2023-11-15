64b5fb7653f434c73c6e5eff845820cfe4bba51c
Michelle Chen <43322845+michelle03029@users.noreply.github.com>
2023-11-14 18:52:44 -0800

    Additions to in class exercise

--- week10/inclass/sketch.js
+++ week10/inclass/sketch.js
@@ -1,10 +1,31 @@
-function setup(){
-    createCanvas(400, 400)
+let numRings = 10
+let radius
+let strWeight = 20
 
+function setup(){
+    createCanvas(800, 800)
+    colorMode(HSB, TWO_PI, 1, 1)
+
+    radius = width * 0.1
 }
 
 function draw(){
-    background(200)
+    background(0)
+    stroke(255,0, 0)
+    noFill()
+    strokeWeight(strWeight)
 
-    circle(width*0.5, height*0.5, 200)
+    for (let i = 0; i < numRings; i ++){
+        push()
+        //translate to the center of screen
+        translate(width*0.5, height*0.5) 
+        //tamer towards the middle, last number is amplitude
+        rotate(sin(millis() * 0.001 * (i*0.5+1) * 0.5))
+        stroke(i*TWO_PI/numRings, 0.8, 0.8)
+        //draw around 0, 0 points
+        arc(0, 0, radius*2 + i*strWeight*2, radius*2 + i*strWeight*2, PI*0.75, PI*0.25) 
+        pop()
+    }
 }
+
+//radius*2 = diameter
---
+++ week10/inclass/sketch_a.js
@@ -0,0 +1,44 @@
+let angle = 0
+
+function setup(){
+    createCanvas(800, 600)
+
+}
+
+function draw(){
+    background (200, 215, 205)
+
+
+    //ROTATING A RECTANGLE
+    fill(225, 190, 210)
+    push()
+    translate(100, 200)
+    rotate(QUARTER_PI)
+    rect(0, 0, 150, 100)
+    pop()
+
+
+    push()
+    translate(width * 0.5, height * 0.5)
+    rotate(angle)
+    fill(180, 230, 205)
+    ellipse(0, 0, 300, 200)
+    pop()
+
+    //rotation
+    angle += 0.01
+    //drawGrid()
+}
+
+function drawGrid(){
+    //DRAWING A GRID
+    for(let x = 0; x <= width; x += (width/10)){
+        stroke(255, 0, 0)
+        line(x, 0, x, height)
+    }
+    
+    for(let y = 0; y <= height; y += (height/10)){
+        stroke(0, 0, 255)
+        line(0, y, width, y)
+    }
+}
